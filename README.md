d9b41f058aa9956f442abef19c845323d12e5f4f
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-10-28 13:12:46 -0300

    Update project setup

---
+++ README.md
@@ -0,0 +1,24 @@
+# kahoot-clone
+
+Kahoot clone using React, Node.js and Socket.io.
+
+## Installation
+
+```
+npm run server-install # Install server-side dependencies
+npm run client-install # Install client-side dependencies
+```
+
+## How to run this project
+
+```
+npm run dev # Run the project in development mode
+```
+
+# Other commands
+
+```
+npm run start-server # Start backend development server
+npm run start-client # Start React development server
+npm run build-client # Create React build
+```
--- back-end/server.js
+++ back-end/server.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const socketIO = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
@@ -15,17 +15,15 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
-
 // DB
-const { Pool, Client } = require('pg')
+const { Pool, Client } = require('pg');
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString: connectionString,
-})
+});
 pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+  console.log(err, res);
+  pool.end();
+});
