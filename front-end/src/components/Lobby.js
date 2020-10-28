2b14804608cc27c9e87d373a8b8d64992b065215
Nicolas Rivarola <RivarolaNicolas94@gmail.com>
2020-10-28 12:19:18 -0300

    Added new functionality to the front-end

--- front-end/src/components/Lobby.js
+++ front-end/src/components/Lobby.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Popover from 'react-bootstrap/Popover';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import StartGame from './StartGame';
+import Players from './Players';
+import StopGame from './StopGame';
 const Lobby = () => {
-  const popover = (
-    <Popover id="popover-basic">
-      <Popover.Title as="h3">Game Status</Popover.Title>
-      <Popover.Content>
-        Game <strong>STARTED</strong>
-      </Popover.Content>
-    </Popover>
-  );
-
   return (
     <div>
-      <OverlayTrigger trigger="click" placement="right" overlay={popover}>
-        <Button>Start Game</Button>
-      </OverlayTrigger>
+      <StartGame />
+      <br />
+      <Players />
+      <br />
+      <br />
+      <br />
+      <br />
+      <StopGame />
     </div>
   );
 };
--- front-end/src/components/Trivia.js
+++ front-end/src/components/Trivia.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Alert from 'react-bootstrap/Alert';
+import StopGame from './StopGame';
+import Questions from './Questions';
+import Countdown from './Countdown';
 const Trivia = () => {
   return (
     <div>
+      <Countdown />
+      <br />
+      <Questions />
       <Alert variant="warning">First option</Alert>
       <Alert variant="warning">Second option</Alert>
       <Alert variant="warning">Third option</Alert>
       <Alert variant="warning">Fourth option</Alert>
+      <br />
+      <StopGame />
     </div>
   );
 };
