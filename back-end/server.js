44c036c65ec9cd11f216ccca16341457d0df0a57
Hernán Peralta <alegodemencia@gmail.com>
2020-10-29 10:35:16 -0300

    Add simple trivia feature

--- back-end/server.js
+++ back-end/server.js
@@ -10,6 +10,19 @@ const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
+const trivia = [
+  {
+    question: "Which is the biggest planet in the Solar System?",
+    options: ["Venus", "Jupiter", "Mercurio", "Mars"],
+    correct: 1,
+  },
+  {
+    question: "Which is the largest animal?",
+    options: ["Cow", "Dog", "Mosquito", "Whale"],
+    correct: 3,
+  }
+];
+
 function updatePlayerList() {
   io.of('/').clients((error, clients) => {
     if (error) throw error;
@@ -21,6 +34,21 @@ function updatePlayerList() {
 io.on('connection', (socket) => {
   console.log('Client connected');
   updatePlayerList()
+
+  let counter = 0;
+  socket.on('next question', () => {
+    if (counter === trivia.length){
+      io.of('/').emit('game ended', 'GAME ENDED!')
+    }
+    else{
+      io.of('/').emit('question', {question: trivia[counter].question, options: trivia[counter].options});
+      counter ++;
+    }
+  });
+
+  socket.on('new game', () => {
+    counter = 0;
+  });
   
   socket.on('disconnect', () => {
     console.log('Client disconnected')
