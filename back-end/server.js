1eec8f8b2dd95dc79ea25480a2719412fe1c86fc
Nicolas Rivarola <RivarolaNicolas94@gmail.com>
2020-10-30 10:25:06 -0300

    Tries to deploy to heroku

--- back-end/server.js
+++ back-end/server.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(express.static('front-end/public'));
+app.use(express.static('front-end/build'));
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
