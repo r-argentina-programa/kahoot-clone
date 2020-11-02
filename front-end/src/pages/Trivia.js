fe3c90b723826809f3bc9617a35b5df0a0155502
Nicolas Rivarola <RivarolaNicolas94@gmail.com>
2020-11-02 09:31:01 -0300

    Update Trivia.js

--- front-end/src/pages/Trivia.js
+++ front-end/src/pages/Trivia.js
@@ -16,16 +16,16 @@ const Trivia = (props) => {
       <Countdown />
       <br />
       <Questions triviaData={props.triviaData} />
-      <Alert className="answer" onClick={() => props.socket.emit('answer', 0)} variant="warning">
+      <Alert className="answer" onClick={() => socket.emit('answer', 0)} variant="warning">
         {props.triviaData.options[0]}
       </Alert>
-      <Alert className="answer" onClick={() => props.socket.emit('answer', 1)} variant="warning">
+      <Alert className="answer" onClick={() => socket.emit('answer', 1)} variant="warning">
         {props.triviaData.options[1]}
       </Alert>
-      <Alert className="answer" onClick={() => props.socket.emit('answer', 2)} variant="warning">
+      <Alert className="answer" onClick={() => socket.emit('answer', 2)} variant="warning">
         {props.triviaData.options[2]}
       </Alert>
-      <Alert className="answer" onClick={() => props.socket.emit('answer', 3)} variant="warning">
+      <Alert className="answer" onClick={() => socket.emit('answer', 3)} variant="warning">
         {props.triviaData.options[3]}
       </Alert>
       <br />
