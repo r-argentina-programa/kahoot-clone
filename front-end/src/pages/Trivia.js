03997ea574a20a2715588bb7915ceb7f05ce4819
Nicolas Rivarola <RivarolaNicolas94@gmail.com>
2020-11-02 11:17:23 -0300

    Adds color to the selected question

--- front-end/src/pages/Trivia.js
+++ front-end/src/pages/Trivia.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import StopGame from '../components/StopGame';
 import Questions from '../components/Questions';
@@ -6,26 +6,66 @@ import Countdown from '../components/Countdown';
 import '../styles/Trivia.css';
 
 const Trivia = (props) => {
+  const [isClicked0, setIsClicked0] = useState(false);
+  const [isClicked1, setIsClicked1] = useState(false);
+  const [isClicked2, setIsClicked2] = useState(false);
+  const [isClicked3, setIsClicked3] = useState(false);
   const socket = props.socket;
   const onGameEnd = props.onGameEnd;
   socket.on('game ended', (podium) => {
     onGameEnd(podium);
   });
+  useEffect(() => {
+    setIsClicked0(false);
+    setIsClicked1(false);
+    setIsClicked2(false);
+    setIsClicked3(false);
+
+    return () => {};
+  }, [props.triviaData]);
   return (
     <div>
       <Countdown />
       <br />
       <Questions triviaData={props.triviaData} />
-      <Alert className="answer" onClick={() => socket.emit('answer', 0)} variant="warning">
+      <Alert
+        className="answer"
+        onClick={() => {
+          setIsClicked0(true);
+          socket.emit('answer', 0);
+        }}
+        variant={isClicked0 ? 'success' : 'warning'}
+      >
         {props.triviaData.options[0]}
       </Alert>
-      <Alert className="answer" onClick={() => socket.emit('answer', 1)} variant="warning">
+      <Alert
+        className="answer"
+        onClick={() => {
+          setIsClicked1(true);
+          socket.emit('answer', 0);
+        }}
+        variant={isClicked1 ? 'success' : 'warning'}
+      >
         {props.triviaData.options[1]}
       </Alert>
-      <Alert className="answer" onClick={() => socket.emit('answer', 2)} variant="warning">
+      <Alert
+        className="answer"
+        onClick={() => {
+          setIsClicked2(true);
+          socket.emit('answer', 0);
+        }}
+        variant={isClicked2 ? 'success' : 'warning'}
+      >
         {props.triviaData.options[2]}
       </Alert>
-      <Alert className="answer" onClick={() => socket.emit('answer', 3)} variant="warning">
+      <Alert
+        className="answer"
+        onClick={() => {
+          setIsClicked3(true);
+          socket.emit('answer', 0);
+        }}
+        variant={isClicked3 ? 'success' : 'warning'}
+      >
         {props.triviaData.options[3]}
       </Alert>
       <br />
