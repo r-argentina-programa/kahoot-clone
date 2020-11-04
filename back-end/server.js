e72cbb756048a94877c05dfa92cf5646e8c75dfe
Hernán Peralta <alegodemencia@gmail.com>
2020-11-04 14:08:16 -0300

    Adds routes for new implementation

--- back-end/server.js
+++ back-end/server.js
@@ -54,191 +54,27 @@ const trivia2 = [
 
 const triviaList = { trivia1, trivia2 };
 
-let rooms = {};
-let socketsInLobby = [];
-
-function joinRoom(socket, room) {
-  room.sockets.push(socket);
+function generatePIN() {
+  return Math.floor(Math.random() * 10000000);
 }
 
-function updateLobbyList(sockets) {
-  const socketsIds = sockets.map((socket) => socket.id);
-  io.emit('players', socketsIds);
-}
+function connectToTrivia(triviaId, io) {
+  const namespace = io.of(`/${triviaId}`);
 
-function calculatePodium(sockets) {
-  let sortedArray = [];
-  let podium = {};
+  return namespace.on('connection', (socket) => {
+    console.log('Client connected');
 
-  sockets.forEach((socket) => {
-    sortedArray.push([socket.id, socket.score]);
-  });
-
-  sortedArray.sort(function (a, b) {
-    return b[1] - a[1];
-  });
-
-  for (let i = 0; i < sortedArray.length; i++) {
-    podium[i] = { name: sortedArray[i][0], score: sortedArray[i][1] };
-  }
-
-  return podium;
-}
-
-function showNextQuestion({ id: roomId, sockets, counter }) {
-  if (counter === trivia.length) {
-    io.to(roomId).emit('game ended', calculatePodium(sockets));
-  } else {
-    sockets.forEach((socket) => {
-      socket.answered = false;
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
     });
-
-    io.to(roomId).emit('question', {
-      question: trivia[counter].question,
-      options: trivia[counter].options,
-    });
-  }
-}
-
-function getAllSockets() {
-  return Object.values(io.sockets.sockets);
-}
-
-function joinRoom(sockets) {
-  const room = {
-    id: uuid.v4(),
-    sockets,
-    counter: 0,
-  };
-
-  sockets.forEach((socket) => socket.join(room.id));
-
-  return room;
-}
-
-function leaveRoom(socket) {
-  for (roomId in rooms) {
-    const room = rooms[roomId];
-    if (room.sockets.includes(socket)) {
-      socket.leave(roomId);
-      room.sockets = room.sockets.filter((skt) => skt !== socket);
-      if (room.sockets.length === 0) {
-        delete rooms[roomId];
-      }
-    }
-  }
-}
-
-function getSocketsInRooms() {
-  if (Object.keys(rooms).length === 0) {
-    return null;
-  }
-
-  const playersInRooms = [];
-
-  for (roomId in rooms) {
-    playersInRooms.push(...rooms[roomId].sockets);
-  }
-
-  return playersInRooms;
-}
-
-function getSocketsInLobby(socketsConnected, socketsInRooms) {
-  if (!socketsConnected || !socketsInRooms) {
-    return null;
-  }
-
-  return socketsConnected.filter((socket) => !socketsInRooms.includes(socket));
-}
-
-function findRoom(socket) {
-  for (roomId in rooms) {
-    const room = rooms[roomId];
-    if (room.sockets.includes(socket)) {
-      return room;
-    }
-  }
-
-  return null;
-}
-
-function initScores(sockets) {
-  sockets.forEach((socket) => {
-    socket.score = 0;
   });
 }
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
+app.get('/trivialist', (req, res) => {
+  res.json({ triviaList: Object.keys(triviaList), pin: generatePIN() });
+});
 
-  const socketsInRooms = getSocketsInRooms();
-
-  if (!socketsInRooms) {
-    socketsInLobby = getAllSockets();
-    updateLobbyList(socketsInLobby);
-  } else {
-    const socketsConnected = getAllSockets();
-    socketsInLobby = getSocketsInLobby(socketsConnected, socketsInRooms);
-    updateLobbyList(socketsInLobby);
-  }
-
-  socket.on('new game', () => {
-    const room = joinRoom(socketsInLobby);
-    rooms[room.id] = room;
-    initScores(room.sockets);
-    showNextQuestion(room);
-    io.to(room.id).emit('toTrivia');
-  });
-
-  socket.on('answer', (index) => {
-    const room = findRoom(socket);
-
-    socket.answered = true;
-
-    if (index === trivia[room.counter].correct) {
-      socket.score += 1;
-    }
-
-    const everyoneAnswered = room.sockets.every((socket) => socket.answered === true);
-
-    if (everyoneAnswered) {
-      room.counter++;
-      showNextQuestion(room);
-    }
-  });
-
-  socket.on('exit', () => {
-    const room = findRoom(socket);
-
-    if (room) {
-      leaveRoom(socket);
-    }
-
-    const socketsInRooms = getSocketsInRooms();
-
-    if (!socketsInRooms) {
-      socketsInLobby = getAllSockets();
-      updateLobbyList(socketsInLobby);
-    } else {
-      const socketsConnected = getAllSockets();
-      socketsInLobby = getSocketsInLobby(socketsConnected, socketsInRooms);
-      updateLobbyList(socketsInLobby);
-    }
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-    leaveRoom(socket);
-
-    const socketsInRooms = getSocketsInRooms();
-
-    if (!socketsInRooms) {
-      socketsInLobby = getAllSockets();
-      updateLobbyList(socketsInLobby);
-    } else {
-      const socketsConnected = getAllSockets();
-      socketsInLobby = getSocketsInLobby(socketsConnected, socketsInRooms);
-      updateLobbyList(socketsInLobby);
-    }
-  });
+app.get('/trivia/:triviaid', (req, res) => {
+  const triviaId = req.params.id;
+  connectToTrivia(triviaId, io);
 });
