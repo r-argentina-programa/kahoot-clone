9239cfeae8a63bb9e5cc9924e5a70522e0412fd5
Nicolas Rivarola <RivarolaNicolas94@gmail.com>
2020-11-09 14:06:22 -0300

    Adds functionality to dinamically render answers

--- front-end/src/components/StopGame.js
+++ front-end/src/components/StopGame.js
@@ -1,17 +1,13 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 const StopGame = (props) => {
   const socket = props.socket;
 
   return (
     <Link to="/">
-      <Button
-        className="stopGame"
-        onClick={() => socket.emit("exit")}
-        variant="danger"
-      >
+      <Button className="stopGame" variant="danger">
         Go back to the Lobby
       </Button>
     </Link>
