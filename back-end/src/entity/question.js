0a979b388aad96045b71c07e3aa6625a0cd9b114
Hernán Peralta <alegodemencia@gmail.com>
2020-11-11 18:24:57 -0300

    Modifies question and trivia entities

--- back-end/src/entity/question.js
+++ back-end/src/entity/question.js
@@ -1,7 +1,8 @@
 module.exports = class Question {
-  constructor({ id, triviaId, description }) {
+  constructor({ id, triviaId, description, answers }) {
     this.id = id;
     this.triviaId = triviaId;
     this.description = description;
+    this.answers = answers;
   }
 };
