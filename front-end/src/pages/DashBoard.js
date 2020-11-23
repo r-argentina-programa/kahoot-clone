0887e3b25934814775c4a0d74cadb9243efb9f44
Nicolas Rivarola <RivarolaNicolas94@gmail.com>
2020-11-23 16:02:19 -0300

    Update DashBoard.js

--- front-end/src/pages/DashBoard.js
+++ front-end/src/pages/DashBoard.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
 
 const DashBoard = () => {
   const [dataLoaded, setDataLoaded] = useState(false);
